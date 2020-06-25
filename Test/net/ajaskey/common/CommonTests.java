b1f409c68d1b509591e663a788788b8be64d96ab
AJAskey <ajaskey34@gmail.com>
2020-06-25 14:09:18 -0600

    Randoop regression test files

--- Test/net/ajaskey/common/CommonTests.java
+++ Test/net/ajaskey/common/CommonTests.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ CommonTests0.class, CommonTests1.class, CommonTests2.class, CommonTests3.class, CommonTests4.class, CommonTests5.class, CommonTests6.class, CommonTests7.class, CommonTests8.class, CommonTests9.class, CommonTests10.class, CommonTests11.class, CommonTests12.class, CommonTests13.class, CommonTests14.class, CommonTests15.class, CommonTests16.class, CommonTests17.class })
+@Suite.SuiteClasses({ CommonTests0.class, CommonTests1.class })
 public class CommonTests {
 }
 
