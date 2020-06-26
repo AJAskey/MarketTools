3212860b16e87a9cb4360da41963c98312ebbdbb
AJAskey <ajaskey34@gmail.com>
2020-06-26 10:07:37 -0600

    Javadoc

--- src/net/ajaskey/market/tools/SIP/BigDB/dataio/FiletypeEnum.java
+++ src/net/ajaskey/market/tools/SIP/BigDB/dataio/FiletypeEnum.java
@@ -18,8 +18,34 @@
  */
 package net.ajaskey.market.tools.SIP.BigDB.dataio;
 
+/**
+ * This enumeration contain the follow types:
+ * 
+ * <p>
+ * <i>NONE</i> - Default type. No processing assigned to this state.
+ * </p>
+ * <p>
+ * <b>BIG_BINARY</b> - Used to process large binary files for each quarter of
+ * SIP and DB data (both input and output).
+ * </p>
+ * <b>BINARY</b> - Used to process small individual binary files of each company
+ * for each year and quarter of data (both input and output).
+ * <p>
+ * <b>TEXT</b> - Used to process small individual text files of each company for
+ * each year and quarter of data (both input and output).
+ * </p>
+ * <p>
+ * Text files can be Gzipped to save space. The reader will process text files
+ * with .gz extension.
+ * </p>
+ * <p>
+ * Binary files contain preprocessed data so that much of read time processing
+ * is skipped. The <b>BIG_BINARY</b> option is the most efficient.
+ * </p>
+ *
+ */
 public enum FiletypeEnum {
 
-  BIG_BINARY, BINARY, NONE, TEXT;
+  NONE, BIG_BINARY, BINARY, TEXT;
 
 }
